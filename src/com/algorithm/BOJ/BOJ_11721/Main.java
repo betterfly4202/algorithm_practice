2bd178f3f89f4fa000684fb72abe43c8ad6d85ad
betterfly <betterfly4202@gmail.com>
2017-12-21 01:54:59 +0900

    - for문 문제까지 클리어

---
+++ src/com/algorithm/BOJ/BOJ_11721/Main.java
@@ -0,0 +1,39 @@
+package com.algorithm.BOJ.BOJ_11721;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by betterFLY on 2017. 12. 21.
+ * Github : http://github.com/betterfly88
+ */
+
+public class Main {
+    public static void main(String [] split){
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        if(A.length()<1 || A.length()>100)
+            return;
+
+        List<String> resultList = setStringList(A);
+        for(int i=0; i<resultList.size();i++){
+            System.out.println(resultList.get(i));
+        }
+    }
+
+
+    public static List<String> setStringList(String insertString){
+        List<String> tempList = new ArrayList<>();
+        int cnt = insertString.length()/10;
+        for(int i=0; i < cnt+1 ;i++){
+            if(insertString.length()>9){
+                tempList.add(insertString.substring(0,10));
+                insertString = insertString.substring(10);
+            }else{
+                tempList.add(insertString);
+            }
+        }
+        return tempList;
+    }
+}
