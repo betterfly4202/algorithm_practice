62b8fd1f08d4894bc3694f1c420f45c96cbaaffb
betterfly <betterfly4202@gmail.com>
2018-01-08 17:36:15 +0900

    - mobigen prepared

---
+++ src/com/algorithm/inflearn/recursion/Mobigen.java
@@ -0,0 +1,14 @@
+package com.algorithm.inflearn.recursion;
+
+/**
+ * Created by betterFLY on 2018-01-08.
+ */
+public class Mobigen {
+    public static void main(String [] mobigen){
+        System.out.println(parseString("a","B"));
+    }
+
+    public static String parseString(String a, String b){
+        return a+b;
+    }
+}
