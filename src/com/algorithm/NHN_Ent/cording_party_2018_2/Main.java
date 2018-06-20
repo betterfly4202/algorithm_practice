0bc403c4e735c5d8f55937a998b3e673e070725d
betterfly <betterfly4202@gmail.com>
2018-06-21 02:10:18 +0900

    to be resolving kakao bank test

--- src/com/algorithm/NHN_Ent/cording_party_2018_2/Main.java
+++ src/com/algorithm/NHN_Ent/cording_party_2018_2/Main.java
@@ -1,12 +1,9 @@
 package com.algorithm.NHN_Ent.cording_party_2018_2;
 
-import javax.print.attribute.standard.NumberUp;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * Created by betterFLY on 2018. 6. 17.
@@ -29,60 +26,58 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> arr = new ArrayList<>();
-        ArrayList<String> resultArr = new ArrayList<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        arr.add(input);
+        String getCanvasSize = br.readLine();
+        String getBgColor = br.readLine();
+        String getFrameNum = br.readLine();
+        String pass = br.readLine();
+        String frameInfo_1 = br.readLine();
+        String frameInfo_2 = br.readLine();
+        String frameInfo_3 = br.readLine();
+        String frameInfo_4 = br.readLine();
+        String frameInfo_5 = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter( System.out ) );
+        ArrayList<String> resultList = new ArrayList<>();
+
+        // 라인 1
+        String canvas [] =getCanvasSize.split("x");
+
+        String canvasX = canvas[0].replaceAll("[^0-9]","");
+        String canvasY = canvas[1].replaceAll("[^0-9]","");
 
 
-        // 입력 받는 걸 수정해서 한줄씩 input.split("\n"); 리스트 뽑고, 파싱해서 결과 처리하기
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        String input = br.readLine();
+        // 라인 2
+        int hexLength = getBgColor.indexOf("0x");
+        String hexStr = getBgColor.substring(hexLength, getBgColor.length());
+        String decValue = getHexToDec(hexStr);
 
-        for(String a : arr){
-            System.out.println(a);
-        }
+        resultList.add(decValue);
 
 
-//        /*
-//            라인 1
-//         */
-//        String canvas [] =arr.get(0).split("x");
-//        resultArr.add(canvas[0].replaceAll("[^0-9]",""));
-//        resultArr.add(canvas[1].replaceAll("[^0-9]",""));
-//        System.out.println("Hello Goorm! Your input is " + input);
-//
-//        /*
-//            라인 2
-//         */
-//        int hexLength = arr.get(1).indexOf("0x");
-//        String hexStr = arr.get(1).substring(hexLength, arr.get(1).length());
-//        String decValue = getHexToDec(hexStr);
-//        System.out.println(decValue);
-//
-//        /*
-//            라인 3
-//         */
-//        resultArr.add(canvas[2].replaceAll("[^0-9]",""));
-//        int frameCount = Integer.parseInt(arr.get(2).replaceAll("[^0-9]",""));
-//
-//        /*
-//            라인 4
-//         */
+        // 라인 3
+        String frameNum = getFrameNum.replaceAll("[^0-9]","");
+
 //        //arr.get(4) 부터 진행
-//        String frameContents [] = arr.get(4).split(" ");
-//
-//        String result = "";
-//        for(int i=0; i< frameCount; i++){
-//            String alpha = frameContents[3].trim().equals("yes") ? "1" : "0";
-//            result = frameContents[1]+"x"+frameContents[2]
-//                    +", "+frameContents[4]
-//                    +", "+alpha;
-//        }
+
+        // 라인 4
+        // TODO : 반복문 처리하기
+        String frameContents [] = frameInfo_1.split(" ");
+
+        String result = "";
+        int frameCount = Integer.parseInt(frameNum);
+        for(int i=0; i< frameCount; i++){
+            String alpha = frameContents[3].trim().equals("yes") ? "1" : "0";
+            result = frameContents[1]+"x"+frameContents[2]
+                    +", "+frameContents[4]
+                    +", "+alpha;
+        }
 //
 //        System.out.println(result);
+
+        for(String result : resultList){
+            System.out.println(result);
+        }
     }
 
     private static String getHexToDec(String hex) {
