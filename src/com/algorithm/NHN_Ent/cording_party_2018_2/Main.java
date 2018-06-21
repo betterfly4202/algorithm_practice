368308256fdb204a387e0d90552f87a890eaa878
betterfly <betterfly4202@gmail.com>
2018-06-22 01:41:38 +0900

    유효성 검사 전

--- src/com/algorithm/NHN_Ent/cording_party_2018_2/Main.java
+++ src/com/algorithm/NHN_Ent/cording_party_2018_2/Main.java
@@ -2,14 +2,12 @@ package com.algorithm.NHN_Ent.cording_party_2018_2;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 /**
  * Created by betterFLY on 2018. 6. 17.
  * Github : http://github.com/betterfly88
  */
-
 
 /*
     Canvas size: 600 x 400
@@ -27,57 +25,58 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String getCanvasSize = br.readLine();
-        String getBgColor = br.readLine();
-        String getFrameNum = br.readLine();
-        String pass = br.readLine();
-        String frameInfo_1 = br.readLine();
-        String frameInfo_2 = br.readLine();
-        String frameInfo_3 = br.readLine();
-        String frameInfo_4 = br.readLine();
-        String frameInfo_5 = br.readLine();
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter( System.out ) );
         ArrayList<String> resultList = new ArrayList<>();
 
         // 라인 1
-        String canvas [] =getCanvasSize.split("x");
+        String getCanvasSize = br.readLine();
 
+        String canvas [] =getCanvasSize.split("x");
         String canvasX = canvas[0].replaceAll("[^0-9]","");
         String canvasY = canvas[1].replaceAll("[^0-9]","");
+        resultList.add(canvasX);
+        resultList.add(canvasY);
 
 
         // 라인 2
+        String getBgColor = br.readLine();
+
         int hexLength = getBgColor.indexOf("0x");
         String hexStr = getBgColor.substring(hexLength, getBgColor.length());
         String decValue = getHexToDec(hexStr);
-
         resultList.add(decValue);
 
-
         // 라인 3
-        String frameNum = getFrameNum.replaceAll("[^0-9]","");
+        String getFrameNum = br.readLine();
 
-//        //arr.get(4) 부터 진행
+        String frameNum =getFrameNum.replaceAll("[^0-9]","");
+        resultList.add(frameNum);
 
-        // 라인 4
-        // TODO : 반복문 처리하기
-        String frameContents [] = frameInfo_1.split(" ");
+        // 라인 4 - Pass
+        String pass = br.readLine();
 
-        String result = "";
-        int frameCount = Integer.parseInt(frameNum);
-        for(int i=0; i< frameCount; i++){
-            String alpha = frameContents[3].trim().equals("yes") ? "1" : "0";
-            result = frameContents[1]+"x"+frameContents[2]
-                    +", "+frameContents[4]
-                    +", "+alpha;
+        // 라인 5
+        int frameCnt = Integer.parseInt(frameNum);
+        for(int i=0; i<frameCnt; i++){
+            String frameInfo = br.readLine();
+            frameInfo = frameInfo.replaceAll("^\\s+","");
+            String frameContents [] = frameInfo.split(" ");
+
+            String parsingResult = getSeperateString(frameContents);
+            resultList.add(parsingResult);
         }
-//
-//        System.out.println(result);
 
         for(String result : resultList){
             System.out.println(result);
         }
+    }
+
+    private static String getSeperateString(String [] frameInfo){
+        String alpha = frameInfo[3].trim().equals("yes") ? "1" : "0";
+        String result = frameInfo[1]+"x"+frameInfo[2]
+                      + ", "+frameInfo[4]
+                      + ", "+alpha;
+
+        return result;
     }
 
     private static String getHexToDec(String hex) {
@@ -86,12 +85,13 @@ public class Main {
         return String.valueOf(v);
     }
 
-    private static boolean isNumber(String str){
+    private static boolean isValidatedNumber(String str){
         boolean result = false;
 
         try{
             int val = Integer.parseInt(str);
             if(val < 0 && val > 65535){
+                //TODO : 숫자 검증 예외 처리
                 throw new Exception();
             }
             return result;
@@ -100,5 +100,11 @@ public class Main {
         }catch (Exception e){
             return result;
         }
+    }
+
+    private static boolean isValidatedString(String str){
+        boolean result = false;
+
+        return result;
     }
 }
