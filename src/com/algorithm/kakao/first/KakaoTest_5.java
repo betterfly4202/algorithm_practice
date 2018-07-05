41307978a865ec5ce6284d3f303ae8205b4f6e6a
betterfly <betterfly4202@gmail.com>
2018-07-05 22:58:35 +0900

    no message

--- src/com/algorithm/kakao/first/KakaoTest_5.java
+++ src/com/algorithm/kakao/first/KakaoTest_5.java
@@ -5,18 +5,19 @@ package com.algorithm.kakao.first;
  */
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
 
      [입력 형식]
 
-     입력으로는 str1과 str2의 두 문자열이 들어온다. 각 문자열의 길이는 2 이상, 1,000 이하이다.
-     입력으로 들어온 문자열은 두 글자씩 끊어서 다중집합의 원소로 만든다. 이때 영문자로 된 글자 쌍만 유효하고, 기타 공백이나 숫자, 특수 문자가 들어있는 경우는 그 글자 쌍을 버린다.
-     예를 들어 “ab+”가 입력으로 들어오면, “ab”만 다중집합의 원소로 삼고, “b+”는 버린다.
-     다중집합 원소 사이를 비교할 때, 대문자와 소문자의 차이는 무시한다. “AB”와 “Ab”, “ab”는 같은 원소로 취급한다.
+     입력으로는 str1과 str2의 두 문자열이 들어온다.
+     - 각 문자열의 길이는 2 이상, 1,000 이하이다.
+     - 입력으로 들어온 문자열은 두 글자씩 끊어서 다중집합의 원소로 만든다.
+     - 이때 영문자로 된 글자 쌍만 유효하고, 기타 공백이나 숫자, 특수 문자가 들어있는 경우는 그 글자 쌍을 버린다.
+       예를 들어 “ab+”가 입력으로 들어오면, “ab”만 다중집합의 원소로 삼고, “b+”는 버린다.
+     - 다중집합 원소 사이를 비교할 때, 대문자와 소문자의 차이는 무시한다. “AB”와 “Ab”, “ab”는 같은 원소로 취급한다.
 
 
      [출력 형식]
@@ -29,21 +30,30 @@ public class KakaoTest_5 {
     public static List<String> str2Arr = new ArrayList<>();
 
     public static void main(String[] args) {
-        String str1 = "FRANCE";
-        String str2 = "FRENCH";
+        String str1 = "handshake";
+        String str2 = "shake hands";
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        if(str1.equals(str2)){
+            System.out.println(65536);
+            return;
+        }
         addStringArray(str1, str1Arr);
         addStringArray(str2, str2Arr);
-
 
         int interCnt = getInterCount(str1Arr, str2Arr);
         int unionCnt = str1Arr.size() + str2Arr.size() - interCnt;
         double calResult = ((double)interCnt/unionCnt) * 65536;
         System.out.println((int)Math.floor(calResult));
+
     }
 
     private static void addStringArray(String str, List<String> arr){
         for(int i=0; i<str.length()-1; i++){
-            arr.add(str.substring(i,i+2));
+            String subStr = str.substring(i,i+2);
+            if(Pattern.matches("^[a-zA-Z]*$", subStr)){
+                arr.add(subStr);
+            }
         }
     }
 
---
+++ src/com/algorithm/kakao/first/KakaoTest_6.java
@@ -0,0 +1,29 @@
+package com.algorithm.kakao.first;
+
+/**
+ * Created by betterFLY on 2018. 7. 1.
+ * Github : http://github.com/betterfly88
+ */
+
+public class KakaoTest_6 {
+
+    /**
+
+     [입력 형식]
+
+     - 입력으로 판의 높이 m, 폭 n과 판의 배치 정보 board가 들어온다.
+     - 2 ≦ n, m ≦ 30
+     - board는 길이 n인 문자열 m개의 배열로 주어진다. 블록을 나타내는 문자는 대문자 A에서 Z가 사용된다.
+
+
+     [출력 형식]
+
+     입력으로 주어진 판 정보를 가지고 몇 개의 블록이 지워질지 출력하라.
+
+
+     */
+
+    public static void main(String [] args){
+
+    }
+}
