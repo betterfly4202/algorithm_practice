2d846058291cfde025e45dce15e86a3e6eacb98b
betterfly <betterfly4202@gmail.com>
2018-07-07 22:55:42 +0900

    문제 요구사항

--- src/com/algorithm/woowahanbros/test/firstTest.java
+++ src/com/algorithm/woowahanbros/test/firstTest.java
@@ -6,4 +6,19 @@ package com.algorithm.woowahanbros.test;
  */
 
 public class firstTest {
+
+    /*
+     *  요구사항
+     *  정수 X (0과 2^20 - 1사이) : 기계가 스택에 X를 푸시
+     *  POP : 기계가 스택에서 맨 위에 번호를 제거
+     *  DUP : 기계가 최상위 번호의 중복을 스택으로 푸시
+     *  + : 스택에서 맨 위에 있는 두개의 원소를 띄우고 합쳐서 합계를 스택에 푸시
+     *  - : 스택에서 두 개의 최상위 요소를 팝하고, 첫 번째(최상위) 요소에서 두 번째 요소를 뺴고 스택에 밀어 넣음
+     *
+     *  모든 작업을 처리 한 후 스택에서 최상위 값을 반환
+     *
+     */
+    public static void main(String [] args){
+
+    }
 }
