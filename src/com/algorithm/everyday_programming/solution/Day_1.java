9b28078316d3c932239e20b73fcb389cc647039a
betterfly <betterfly4202@gmail.com>
2018-07-30 00:21:25 +0900

    no message

---
+++ src/com/algorithm/everyday_programming/solution/Day_1.java
@@ -0,0 +1,31 @@
+package com.algorithm.everyday_programming.solution;
+
+/**
+ * Created by betterFLY on 2018. 7. 30.
+ * Github : http://github.com/betterfly88
+ */
+
+
+/*
+    정수 배열(int array)가 주어지면 가장 큰 이어지는 원소들의 합을 구하시오. 단, 시간복잡도는 O(n).
+
+    예제}
+
+    Input: [-1, 3, -1, 5]
+
+    Output: 7 // 3 + (-1) + 5
+
+
+
+    Input: [-5, -3, -1]
+
+    Output: -1 // -1
+
+
+
+    Input: [2, 4, -2, -3, 8]
+
+    Output: 9 // 2 + 4 + (-2) + (-3) + 8
+ */
+public class Day_1 {
+}
