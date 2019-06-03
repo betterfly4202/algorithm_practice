ebab1d42c4ee21cde8a24939865ec374eac4ee0e
이충일 <betterfly4202@gmail.com>
2019-06-04 08:54:01 +0900

    no message

--- src/main/java/com/algorithm/hackerrank/greedy/MinimumAbsoluteDifference.java
+++ src/main/java/com/algorithm/hackerrank/greedy/MinimumAbsoluteDifference.java
@@ -1,14 +1,15 @@
 package com.algorithm.hackerrank.greedy;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class Solution {
+/**
+ * Created by betterfly
+ * Date : 2019.06.04
+ */
+public class MinimumAbsoluteDifference {
 
     // Complete the minimumAbsoluteDifference function below.
     static int minimumAbsoluteDifference(int[] arr) {
--- src/main/java/com/algorithm/hackerrank/regex_2/Solutions.java
+++ src/main/java/com/algorithm/hackerrank/regex_2/Solutions.java
@@ -32,31 +32,28 @@ public class Solutions {
 
     }
 
-//    public void duplicateWords(){
-//        String regex = "/* Write a RegEx matching repeated words here. */";
-//        Pattern p = Pattern.compile(regex, /* Insert the correct Pattern flag here.*/);
-//
-//        Scanner in = new Scanner(System.in);
-//        int numSentences = Integer.parseInt(in.nextLine());
-//
-//        while (numSentences-- > 0) {
-//            String input = in.nextLine();
-//
-//            Matcher m = p.matcher(input);
-//
-//            // Check for subsequences of input that match the compiled pattern
-//            while (m.find()) {
-//                input = input.replaceAll(
-//                        /* The regex to replace */, /* The replacement. */
-//                );
-//            }
-//
-//            // Prints the modified sentence.
-//            System.out.println(input);
-//        }
-//
-//        in.close();
-//    }
+    public void duplicateWords(){
+        String regex = "/* Write a RegEx matching repeated words here. */";
+        Pattern p = Pattern.compile(regex+"/* Insert the correct Pattern flag here.*/");
 
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
 
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+
+            Matcher m = p.matcher(input);
+
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll(
+                        ""/* The regex to replace */, ""/* The replacement. */
+                );
+            }
+
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+        in.close();
+    }
 }
