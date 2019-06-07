d1d134aba4276c81bbb0f237d903ea64fb6937a6
이충일 <betterfly4202@gmail.com>
2019-06-07 22:06:10 +0900

    no message

---
+++ src/main/java/com/algorithm/hackerrank/greedy/MarcsCakeWalk.java
@@ -0,0 +1,51 @@
+package com.algorithm.hackerrank.greedy;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**
+ * Created by betterfly
+ * Date : 2019.06.04
+ */
+public class MarcsCakeWalk {
+
+    // Complete the marcsCakewalk function below.
+    static long marcsCakewalk(int[] calorie) {
+
+        return 1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] calorie = new int[n];
+
+        String[] calorieItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int calorieItem = Integer.parseInt(calorieItems[i]);
+            calorie[i] = calorieItem;
+        }
+
+        long result = marcsCakewalk(calorie);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
