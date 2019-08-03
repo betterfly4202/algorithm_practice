db33750167432112174d4448eaf4b58b75ace165
이충일 <betterfly4202@gmail.com>
2019-08-03 18:39:45 +0900

    no message

--- src/main/java/com/algorithm/hackerrank/search/MissingNumber.java
+++ src/main/java/com/algorithm/hackerrank/search/MissingNumber.java
@@ -1,10 +1,125 @@
 package com.algorithm.hackerrank.search;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by betterfly
  * Date : 2019.08.01
  * URL : https://www.hackerrank.com/challenges/missing-numbers/problem
  */
 public class MissingNumber {
-    
+    private static int maxNum = 0;
+    private static int minNum = 0;
+
+    static int[] missingNumbers(int[] arr, int[] brr) {
+        int oriIdx = 0;
+        int missIdx = 0;
+        List<Integer> resultArr = new ArrayList<>();
+        minNum = brr[0];
+
+        for (int aBrr : brr) {
+            compareMaxNum(aBrr);
+            compareMinNum(aBrr);
+        }
+
+        if (maxNum - minNum > 100) {
+            throw new IllegalArgumentException("The difference between maximum and minimum number over 100.");
+        }
+
+        while(oriIdx< brr.length){
+            int cValue = brr[oriIdx];
+
+            if (missIdx >= arr.length
+             || cValue != arr[missIdx]){
+                if(!resultArr.contains(cValue)){
+                    resultArr.add(cValue);
+                }
+
+                oriIdx++;
+                continue;
+            }
+
+            missIdx++;
+            oriIdx++;
+        }
+        Collections.sort(resultArr);
+        return listToArray(resultArr);
+    }
+
+
+    private static void compareMaxNum(int val){
+        if(maxNum < val){
+            maxNum = val;
+        }
+    }
+
+    private static void compareMinNum(int val){
+        if(minNum > val){
+            minNum = val;
+        }
+    }
+
+    private static int [] listToArray(List<Integer> list){
+        int [] result = new int[list.size()];
+
+        for(int i=0; i<list.size(); i++){
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int m = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] brr = new int[m];
+
+        String[] brrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int brrItem = Integer.parseInt(brrItems[i]);
+            brr[i] = brrItem;
+        }
+
+        int[] result = missingNumbers(arr, brr);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
--- src/test/java/com/algorithm/hackerrank/search/MissingNumberTest.java
+++ src/test/java/com/algorithm/hackerrank/search/MissingNumberTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -17,9 +19,11 @@ public class MissingNumberTest {
 
     private List<Integer> missingArr = new ArrayList<>();
     private List<Integer> originArr = new ArrayList<>();
+    private List<Integer> resultArr = new ArrayList<>();
+
     @Before
     public void init(){
-        originArr = Arrays.asList(203,204,204,205,206,207,205,208,203,206,205,206,204);
+        originArr = Arrays.asList(203,204,204,205,206,207,205,208,203,206,205,206,204); // indx :
         missingArr = Arrays.asList(203,204,205,206,207,208,203,204,205,206);
     }
 
@@ -39,21 +43,49 @@ public class MissingNumberTest {
 
     }
 
+
+    /*
+        originArr =  Arrays.asList(203,204,204,205,206,207,205,208,203,206,205,206,204);
+        missingArr = Arrays.asList(203,204,205,206,207,208,203,204,205,206);
+     */
     @Test
     public void 순차접근(){
-        List<Integer> resultArr = new ArrayList<>();
-        int idx = 0;
+        int oriIdx = 0;
+        int missIdx = 0;
 
-        while(idx < missingArr.size()){
-            int cValue = originArr.get(idx);
-            if (cValue != missingArr.get(idx)){
-                resultArr.add(cValue);
+        while(oriIdx< originArr.size()){
+            if (missIdx > missingArr.size()){
+                return;
+            }
+
+            int cValue = originArr.get(oriIdx);
+
+            if (cValue != missingArr.get(missIdx)){
+                if(!resultArr.contains(cValue)){
+                    resultArr.add(cValue);
+                }
+
+                oriIdx++;
                 continue;
             }
 
-            idx++;
+            missIdx++;
+            oriIdx++;
         }
 
+        // 204 205 206
+        Collections.sort(resultArr);
         resultArr.forEach(System.out::println);
+    }
+
+    @Test
+    public void 검증(){
+        int [] arr = new int [] {203,204,204,205,206,207,205,208,203,206,205,206,204};
+        int [] brr = new int [] {203,204,205,206,207,208,203,204,205,206};
+
+//        int [] arr =new int [] {11,4,11,7,13,4,12,11,10,14};
+//        int [] brr =new int [] {11,4,11,7,3,7,10,13,4,8,12,11,10,14,12};
+        int [] aa = MissingNumber.missingNumbers(arr, brr);
+
     }
 }
