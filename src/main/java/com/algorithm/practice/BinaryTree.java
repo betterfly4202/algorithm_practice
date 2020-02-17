531226d279f693313bdae6df29409ce4059223b6
betterfly <betterfly4202@gmail.com>
2020-02-17 10:34:14 +0900

    no message

---
+++ src/main/java/com/algorithm/practice/BinaryTree.java
@@ -0,0 +1,19 @@
+package com.algorithm.practice;
+
+public class BinaryTree {
+    class Node{
+        private int data;
+        private Node node;
+
+        Node(int data){
+            this.data = data;
+        }
+
+        Node(Node node){
+            this.node = node;
+        }
+    }
+    public static int solution(int [] A){
+        return 0;
+    }
+}
---
+++ src/test/java/com/algorithm/practice/BinaryTreeTest.java
@@ -0,0 +1,7 @@
+package com.algorithm.practice;
+
+import static org.junit.Assert.*;
+
+public class BinaryTreeTest {
+
+}
