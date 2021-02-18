16bf2a96b6b7a51da9a1e4c5040989b895d572f3
betterfly <betterfly4202@gmail.com>
2021-02-18 21:38:59 +0900

    no message

---
+++ src/main/java/com/algorithm/hackerrank/sorting/theFullCountingSort/Solution.java
@@ -0,0 +1,15 @@
+package com.algorithm.hackerrank.sorting.theFullCountingSort;
+
+/*
+    https://www.hackerrank.com/challenges/countingsort4/problem?h_r=internal-search
+ */
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+import java.util.List;
+
+public class Solution {
+    void countSort(List<List<String>> arr) {
+
+
+    }
+}
---
+++ src/test/java/com/algorithm/hackerrank/sorting/theFullCountingSort/SolutionTest.java
@@ -0,0 +1,16 @@
+package com.algorithm.hackerrank.sorting.theFullCountingSort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void NullPointer_에러_발생(){
+        String helloWorld = null;
+
+        System.out.println(helloWorld.toString());
+    }
+
+}
