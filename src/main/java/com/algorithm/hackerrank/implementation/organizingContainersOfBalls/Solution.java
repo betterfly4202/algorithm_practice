8d4d472eda312b5fc63186fb486e1db07a37b733
betterfly <betterfly@wemakeprice.com>
2021-04-22 18:04:06 +0900

    organizingContainersOfBalls

---
+++ src/main/java/com/algorithm/hackerrank/implementation/organizingContainersOfBalls/Solution.java
@@ -0,0 +1,11 @@
+package com.algorithm.hackerrank.implementation.organizingContainersOfBalls;
+
+import java.util.List;
+
+public class Solution {
+    public static String organizingContainers(List<List<Integer>> container) {
+        // Write your code here
+
+        return null;
+    }
+}
