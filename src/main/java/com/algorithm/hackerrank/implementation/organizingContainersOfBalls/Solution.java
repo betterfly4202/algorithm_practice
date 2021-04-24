9fc13131813995c777a5ff8b6ca24c1558edd83f
betterfly <betterfly@wemakeprice.com>
2021-04-24 16:20:24 +0900

    countingSort

--- src/main/java/com/algorithm/hackerrank/implementation/organizingContainersOfBalls/Solution.java
+++ src/main/java/com/algorithm/hackerrank/implementation/organizingContainersOfBalls/Solution.java
@@ -2,6 +2,7 @@ package com.algorithm.hackerrank.implementation.organizingContainersOfBalls;
 
 import java.util.List;
 
+// https://www.hackerrank.com/challenges/organizing-containers-of-balls/problem
 public class Solution {
     public static String organizingContainers(List<List<Integer>> container) {
         // Write your code here
