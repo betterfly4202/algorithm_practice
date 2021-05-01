0c15a8c674fcfdcceeaa45d170ceaf59ab9509f7
betterfly <betterfly@wemakeprice.com>
2021-05-01 16:53:00 +0900

    resolved countingSort

--- src/main/java/com/algorithm/hackerrank/sorting/countingSort/Solution.java
+++ src/main/java/com/algorithm/hackerrank/sorting/countingSort/Solution.java
@@ -2,18 +2,20 @@ package com.algorithm.hackerrank.sorting.countingSort;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // https://www.hackerrank.com/challenges/countingsort1/problem
 public class Solution {
     public static List<Integer> countingSort(List<Integer> arr) {
+
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i = 0; i < 100; i++) {
             result.add(0);
         }
 
-        for (Integer num : arr) {
-            result.set(num, result.get(num)+1);
+        for (int i = 0; i < arr.size(); i++) {
+            result.set(arr.get(i), result.get(arr.get(i))+1);
         }
 
         return result;
