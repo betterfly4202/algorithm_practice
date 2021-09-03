d9912ce175f8a1c801639bab55a759a165a71d31
betterfly <betterfly4202@gmail.com>
2021-09-04 02:20:11 +0900

    gradle version

---
+++ src/test/java/com/algorithm/kakao/second/RecommendIdTest.java
@@ -0,0 +1,9 @@
+package com.algorithm.kakao.second;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecommendIdTest {
+}
